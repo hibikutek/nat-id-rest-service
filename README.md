f7f3481faf5eceec30166dd7262546c3a11a16bb
hibikutek <hibikutek@gmail.com>
2022-04-18 16:46:55 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# National ID RESTful Service
+This is a service for that processes and persists National IDs.
---
+++ src/test/java/org/telesoftas/natidrestservice/NatIdRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telesoftas.natidrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatIdRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
